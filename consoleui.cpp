12b0fc66fe4404f5a7f987dad1675f822f10ac71
Thorri <kristinn.thorri@gmail.com>
2014-11-27 22:32:29 +0000

    Skrifar uppls um persónu í file (loopar meðan user vill halda áfram)

--- consoleui.cpp
+++ consoleui.cpp
@@ -1,5 +1,7 @@
 #include "consoleui.h"
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "person.h"
 
 using namespace std;
@@ -10,7 +12,12 @@ ConsoleUI::ConsoleUI() {
 
 void ConsoleUI::start() {
     Person p1;
+    string line;
+    ofstream outputFile;
+    outputFile.open("test.txt");
     char c;
+
+
 
     do
     {
@@ -19,9 +26,22 @@ void ConsoleUI::start() {
 
     // add person to file
 
+    outputFile << p1;
+
+    // add person to file
+
     cout << "Og þetta er það sem prentast:" << endl;
     cout << p1;
     cout << "Ready to add another? (y/n?) ";
     cin >> c;
     }while(c != 'N' && c != 'n');
+
+    cout << "Nuna prentum við ur filenum" << endl;
+    ifstream myfile ("test.txt");
+    while (getline (myfile, line))
+    {
+        cout << line << endl;
+    }
+    outputFile.close();
+    myfile.close();
 }
