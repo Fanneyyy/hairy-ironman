06f388e7a9a831c697275533a841260479e87b53
Thorri <kristinn.thorri@gmail.com>
2014-12-12 10:23:13 +0000

    :D

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "mainwindow.h"
+#include "personsservice.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
+    PersonsService p;
+
+    p.setUp();
+    qDebug() << p.getSizeOfRepo();
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
