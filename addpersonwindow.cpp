d8ea2c51567efafc8d29ffd792c9e3ada1ad8b99
evaingibjorg <evaingibjorg@gmail.com>
2014-12-16 11:19:18 +0000

    laga add person

--- addpersonwindow.cpp
+++ addpersonwindow.cpp
@@ -6,7 +6,6 @@ AddPersonWindow::AddPersonWindow(QWidget *parent) :
     ui(new Ui::AddPersonWindow) {
     ui->setupUi(this);
     ui->lineFullname->setPlaceholderText("Please enter full name");
-    ui->lineGender->setPlaceholderText("Please enter gender (male/female)");
     ui->lineYearOfBirth->setPlaceholderText("Please enter the year of birth (Example: 1994)");
     ui->lineYearOfDeath->setPlaceholderText("Please enter the year of death (Example: 1999)");
 }
@@ -18,7 +17,6 @@ AddPersonWindow::~AddPersonWindow() {
 void AddPersonWindow::on_AddPersonButton_clicked() {
     addPerson();
     ui->lineFullname->clear();
-    ui->lineGender->clear();
     ui->lineYearOfBirth->clear();
     ui->lineYearOfDeath->clear();
     close();
@@ -31,8 +29,6 @@ void AddPersonWindow::addPerson() {
 
     temp = ui->lineFullname->text().toStdString();
     p.setName(temp);
-    temp = ui->lineGender->text().toStdString();
-    p.setGender(temp);
     temp = ui->lineYearOfBirth->text().toStdString();
     p.setYearOfBirth(temp);
     temp = ui->lineYearOfDeath->text().toStdString();
